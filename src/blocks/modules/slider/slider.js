862e08e3a6c7ba59315a67b4eb0b5b14a1dd84da
Liza Elkina <lizapetkova@yandex.ru>
2026-09-15 17:45:34 +0300

    feat: swiper-pagination

--- src/blocks/modules/slider/slider.js
+++ src/blocks/modules/slider/slider.js
@@ -15,6 +15,14 @@ document.addEventListener('DOMContentLoaded', () => {
         prevEl: '.js-slider-prev',
       },
 
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + (index + 1) + '</span>';
+        },
+      },
+
       keyboard: {
         enabled: true,
         onlyInViewport: false,
