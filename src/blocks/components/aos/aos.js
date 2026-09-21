45d02078bcbae0fee5ba603fa2e83a91eb16c003
Liza Elkina <lizapetkova@yandex.ru>
2026-09-21 21:22:33 +0300

    feat: clubs ranking animate on scroll

---
+++ src/blocks/components/aos/aos.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line no-undef
+AOS.init({
+  once: true,
+  duration: 1000,
+  easing: 'ease',
+});
--- src/js/import/components.js
+++ src/js/import/components.js
@@ -1,1 +1,2 @@
+import '%components%/aos/aos';
 import '%components%/burger/burger';
