dd1d8019b4b4372d74a4c6ef349e4b3b9554e6f3
Liza Elkina <lizapetkova@yandex.ru>
2026-09-24 19:22:49 +0300

    feat: lazy loading libs

--- src/blocks/components/aos/aos.js
+++ src/blocks/components/aos/aos.js
@@ -1,7 +1,9 @@
-// eslint-disable-next-line no-undef
-AOS.init({
-  once: true,
-  duration: 1000,
-  easing: 'ease',
-  disable: window.matchMedia('(prefers-reduced-motion: reduce)').matches,
+document.addEventListener('DOMContentLoaded', () => {
+  // eslint-disable-next-line no-undef
+  AOS.init({
+    once: true,
+    duration: 1000,
+    easing: 'ease',
+    disable: window.matchMedia('(prefers-reduced-motion: reduce)').matches,
+  });
 });
